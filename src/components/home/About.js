5808c31cc949ff3624ac3ea402222c0339b14495
Samia Senzuti <ssen0025@student.monash.edu>
2024-05-05 09:19:40 +1000

    deploy

--- src/components/home/About.js
+++ src/components/home/About.js
@@ -53,21 +53,21 @@ export default function About() {
                   <img
                     className="img-fluid rounded w-100 wow zoomIn"
                     data-wow-delay="0.3s"
-                    src={require("/assets/img/about-2.jpg").default}
+                    src={require("./assets/img/about-2.jpg")}
                   />
                 </div>
                 <div className="col-6 text-end">
                   <img
                     className="img-fluid rounded w-50 wow zoomIn"
                     data-wow-delay="0.5s"
-                    src={require("/assets/img/about-3.jpg").default}
+                    src={require("./assets/img/about-3.jpg")}
                   />
                 </div>
                 <div className="col-6 text-start">
                   <img
                     className="img-fluid rounded w-75 wow zoomIn"
                     data-wow-delay="0.7s"
-                    src={require("/assets/img/about-4.jpg").default}
+                    src={require("./assets/img/about-4.jpg")}
                   />
                 </div>
               </div>
